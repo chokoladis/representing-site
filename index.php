9f0ca82a92e5a43a1d19977b79d04e2fca3c2a2a
chokoladis <igor2011__igor@mail.ru>
2024-08-11 16:10:25 +0400

    little fix, add gen.meme

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chokoladis stuff</title>
+    <meta name="keywords" content="chokoladis, работы chokoladis">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <main>
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="https://github.com/chokoladis/portfolio">portfolio</a>
+                    <div class="blur"></div>
+                </li>
+                <li>
+                    <a href="https://github.com/chokoladis/strange-question">what.if</a>
+                    <div class="blur"></div>
+                </li>
+                <li>
+                    <a href="/gen.meme/">meme.gen</a>
+                    <div class="blur"></div>
+                </li>
+            </ul>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            <i>p.s. coming soon</i>
+            <p>for contacts <a href="https://t.me/chokoladis">telegram</a></p>
+            <div class="copyright">copyright 2024y.</div>
+        </div>
+    </footer>
+</body>
+</html>
